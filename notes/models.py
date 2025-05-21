67c06a8ff86e64801686557698b19d3e81ce4e36
Sergio Inzunza <inxunxa@gmail.com>
2025-05-20 19:11:59 -0700

    Notes list

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=70)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="notes/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
